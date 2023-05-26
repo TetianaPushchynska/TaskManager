fa259963b28257e88da134bb06e5c731b2e0f655
TetianaPushchynska <tanya.pushchynska@gmail.com>
2023-05-26 11:59:10 +0300

    [TM-3] Added and configured gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'users', path_names: { sign_in: 'sign_in', sign_out: 'sign_out' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "tasks#index"
+
+  resources :tasks
 end
