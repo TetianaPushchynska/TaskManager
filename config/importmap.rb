a92972a2e0d3f5f7cf15635ef99122462fc42abb
Tetiana Pushchynska <tanya.pushchynska@gmail.com>
2023-05-29 17:43:11 +0200

    [TM-11] Fixed small bugs (#10)

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,9 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "jquery", to: "https://code.jquery.com/jquery-3.7.0.min.js", preload: true
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
